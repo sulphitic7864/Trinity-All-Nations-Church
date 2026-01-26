fcfd751b981cdf7ef72e48e8d67458a744c6089c
waseemQasim786 <waseem.qasim@innovotechnologies.com>
2026-01-27 01:00:49 +0500

    motion framer is updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, JSX } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion/client";
 import {
   Menu,
   X,
